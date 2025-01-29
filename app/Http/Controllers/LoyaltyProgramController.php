33b87d43cdfda4718b48a7289efb4a18195a4eee
NikolaevNS1995 <nikolaevns1995@gmail.com>
2025-01-29 17:40:44 +0300

    Controllers

---
+++ app/Http/Controllers/LoyaltyProgramController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLoyaltyProgramRequest;
+use App\Http\Requests\UpdateLoyaltyProgramRequest;
+use App\Models\LoyaltyProgram;
+
+class LoyaltyProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLoyaltyProgramRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LoyaltyProgram $loyaltyProgram)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LoyaltyProgram $loyaltyProgram)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLoyaltyProgramRequest $request, LoyaltyProgram $loyaltyProgram)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LoyaltyProgram $loyaltyProgram)
+    {
+        //
+    }
+}
